132ff3da773456c760895a79bac4d8cd5daa7738
rinkoko <gnaao1005@gmail.com>
2025-03-11 21:49:59 +0900

    Update 완전 이진 트리.py
    added other annotation

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
@@ -20,3 +20,24 @@ find_depth(in_order, 0)
 
 for floor in complete_binary_tree:
     print(*floor)
+
+# 포화 이진 트리가 아닐 때 root 찾기
+# # 층마다 몇 개씩인지 세기
+# N = len(in_order)
+# M = N
+# H = [] # 층마다 몇 개씩 있는지
+# for i in range(K):
+#     n = 2**i
+#     if n>M:
+#         H.append(M)
+#         break
+#     H.append(n)
+#     M -= n
+#
+# root = 1
+# for j in range(K-1, 0, -1):
+#     left = (2**j)//2
+#     if H[j]<left:
+#         root += H[j]
+#     else:
+#         root += left
