4549c752a17f16bcd12e2577f627316809dc95bb
Xiumin1714 <ksm1714@snu.ac.kr>
2025-03-13 12:52:38 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 96 ms, Memory: 59,904 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,18 +1,21 @@
-def f(k, N, M):         # 이전까지의 교환횟수
-    global max_v
-    if k == N:          # 주어진 교환 횟수 사용
+def f(i, M, N): # i 카드 위치, M 카드 수, N 남은교환횟수
+    global max_v, min_cnt   # max_v를 찾았을 때의 최소 교환횟수 min_cnt
+    if i == M or N == 0:      # 모든 자리를 결정했거나 교환횟수가 다 소진됐으면
         tmp = int(''.join(card))
-        if max_v < tmp:
+        if max_v <= tmp:
             max_v = tmp
+            if min_cnt[max_v] > N:
+                min_cnt[max_v] = N
+
     else:
-        for i in range(M-1):
-            for j in range(i+1, M):
+        for j in range(M):
+            if i != j:      # 교환
                 card[i], card[j] = card[j], card[i]
-                tmp = int(''.join(card))
-                if tmp not in memo[k]:
-                    memo[k].append(tmp)
-                    f(k+1, N, M)
-                card[i], card[j] = card[j], card[i]     # 가능한 교환을 수행한 뒤 원상복구
+                f(i+1, M, N-1)  # 교환을 한 번 했으니 N -> N-1
+                card[i], card[j] = card[j], card[i]
+            else:           # i == j # 교환 횟수를 다른 자리에서 사용하도록 넘겨줌
+                f(i+1, M, N)
+
 
 T = int(input())
 
@@ -22,8 +25,15 @@ for tc in range(1, T+1):
     N = int(N)
     M = len(card)
     max_v = 0           # 최대 상금
-    memo = [[] for _ in range(N)]     # memo[k]행이 k번 교환에서 만들어지는 숫자를 저장
+    min_cnt = [10]*(10**M)       # max_v가 갱신됐을 때의 교환 횟수
 
-    f(0, N, M)
+    f(0, M, N)
+
+    if min_cnt[max_v]%2:      # 남은 교환횟수가 있고, 그 횟수가 홀수면
+        card[-1], card[-2] = card[-2], card[-1]
+        n1 = max_v % 10       # 1의자리
+        n10 = max_v % 100 // 10 # 10의 자릿수
+        max_v = max_v // 100*100 + n1*10 + n10
+
 
     print(f'#{tc} {max_v}')
