0f08588ca05249e405a4d4ab525d6fbcf24a7d29
Jin <bblinging22@gmail.com>
2025-03-20 17:03:06 +0900

    [Gold IV] Title: 최단경로, Time: 500 ms, Memory: 138556 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,40 @@
+# BOJ 1753
+import heapq
+
+def dijkstra(s_node):
+    pq = [(0, s_node)]
+    ds = [float('inf')]*(V+1)
+    ds[s_node] = 0      # 시작점 자신은 0
+
+    while pq:
+        d, node = heapq.heappop(pq)
+        if ds[node] < d:
+            continue
+
+        for nex_info in graph[node]:
+            next_d = nex_info[0]
+            next_node = nex_info[1]
+            new_d = d + next_d
+
+            if ds[next_node] <= new_d:
+                continue
+
+            ds[next_node] = new_d
+            heapq.heappush(pq, (new_d, next_node))
+
+    return ds[1:]
+
+V, E = map(int, input().split())
+K = int(input())
+s_node = K
+graph = [[] for _ in range(V+1)]
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append((w, v))
+
+result = dijkstra(K)
+for i in range(V):
+    if result[i] == float('inf'):
+        print('INF')
+    else:
+        print(result[i])
