0721853bfb583832c7eaf7970c1a97701c4094b7
leeaethra <strangem111n@gmail.com>
2025-03-24 12:04:11 +0900

    3월 3주차

--- 백준/Bronze/2729. 이진수 덧셈/이진수 덧셈.py
+++
@@ -1,7 +0,0 @@
-T = int(input())
-for _ in range(T):
-    a, b = input().split()
-    a = int(a,2)
-    b = int(b,2)
-    result = bin(a+b)
-    print(result[2:])
--- 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++
@@ -1,8 +0,0 @@
--- 코드를 작성해주세요
-SELECT E.ID, COUNT(E2.ID) AS CHILD_COUNT -- 자식 수를 출력
-FROM ECOLI_DATA E
--- 자식이 없어도(NULL)이어도 출력되도록
-LEFT JOIN ECOLI_DATA E2 ON E.ID = E2.PARENT_ID 
--- 같은 테이블 조인해서 있는 ID 번호를 E.2에 가져오기
-GROUP BY E.ID -- GROUP BY가 꼭 있어야함?
-ORDER BY E.ID ASC;    -- 개체 ID 에 대해 오름차순
