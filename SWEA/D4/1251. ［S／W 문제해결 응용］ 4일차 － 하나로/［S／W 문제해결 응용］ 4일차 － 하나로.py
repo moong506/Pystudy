e2db2198ac1ea52ac03080cf19849eedf748a3fa
Xiumin1714 <ksm1714@snu.ac.kr>
2025-03-25 08:08:27 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 284 ms, Memory: 73,600 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.py
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.py
@@ -1,67 +1,48 @@
-# kruskal은 간선 위주 알고리즘
-# 간선들을 정렬하고 최소 간선을 뽑자 이때, 사이클을 검사하자
-
-# kruskal 버전
 def find_set(x):
     if x == parents[x]:
         return x
-
-    # 기본코드 : 경로 압축 안 한 버전
-    # return find_set(parents[x])
-
-    # 경로 압축 코드
     parents[x] = find_set(parents[x])
     return parents[x]
-
-
+ 
 def union(x, y):
     ref_x = find_set(x)
     ref_y = find_set(y)
-
     if ref_x == ref_y:
         return
-
-    # 기본 연결 코드
-    # parents[ref_y] = ref_x
-
-    # 디버깅용 or 문제 조건에 따라 설정
     if ref_x < ref_y:
         parents[ref_y] = ref_x
     else:
         parents[ref_x] = ref_y
-
-
+ 
+ 
 T = int(input())
-
-for tc in range(1, T + 1):
+for tc in range(1, T+1):
     N = int(input())
-    x_list = list(map(int, input().split()))
-    y_list = list(map(int, input().split()))
-    tax = float(input())
-
+    x_lst = list(map(int, input().split()))     # 섬의 x좌표
+    y_lst = list(map(int, input().split()))     # 섬의 y좌표
+    tax = float(input())    # 환경 부담 세율
+ 
     parents = [i for i in range(N)]
     min_cost = 0
-
-    # 1. 간선들 정보를 모두 저장
+ 
+    # 1. 간선 정보 저장
     edges = []
     for i in range(N):
-        for j in range(i + 1, N):
-            cost = ((x_list[i] - x_list[j]) ** 2 +
-                    (y_list[i] - y_list[j]) ** 2) * tax
+        for j in range(i+1, N):
+            cost = ((x_lst[i]-x_lst[j])**2 + (y_lst[i]-y_lst[j])**2) * tax
             edges.append((i, j, cost))
-
-    # 2. 가중치 기준으로 오름차순 정렬
-    edges.sort(key=lambda x: x[2])
-
-    # 3. 싸이클 검사하면서, 앞에서부터 간선을 연결한다.
-    #  - 언제까지 반복? N-1 개의 간선이 선택될 때 까지
-    count = 0
+ 
+    # 2. 가중치 기준 오름차순 정렬
+    edges.sort(key = lambda x: x[2])
+ 
+    # 3. 앞에서부터 간선 검사하면서, 사이클 유무 검사 (N-1)개의 간선을 선택하면 종료
+    cnt = 0
     for u, v, w in edges:
         if find_set(u) != find_set(v):
             union(u, v)
             min_cost += w
-            count += 1
-        if count == N - 1:
+            cnt += 1
+        if cnt == N-1:
             break
-
-    print(f'#{tc} {min_cost:.0f}')
+ 
+    print(f'#{tc} {min_cost: .0f}')
